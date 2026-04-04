25b0be53c130a97d415b67ff808e4b9f72c7d7b1
Marqui <bmay86@protonmail.com>
2026-04-04 18:07:01 -0400

    Connect db file and import dotenv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+await connectDB();
 app.use(express.json());
 app.use("/api/notes", notesRoutes);
 
