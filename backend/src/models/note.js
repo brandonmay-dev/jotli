463a7deba6a4c7d93a5a76501f38920c44dfdd6f
Marqui <bmay86@protonmail.com>
2026-04-04 18:47:56 -0400

    Create Note model with title and content schema

---
+++ backend/src/models/note.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+// schema
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+// model
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
