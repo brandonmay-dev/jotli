b4b62eeca44cc42094179b5b23ba97f60f9e1242
Marqui <bmay86@protonmail.com>
2026-04-04 16:59:50 -0400

    Clean up formatting in notesRoutes file

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -9,11 +9,8 @@ import {
 const router = express.Router();
 
 router.get("/", getAllNotes);
-
 router.post("/", createNote);
-
 router.put("/:id", updateNote);
-
 router.delete("/:id", deleteNote);
 
 export default router;
