3702a13a66d39c0c2a81d3ac19dc965cd22002c9
Marqui <bmay86@protonmail.com>
2026-04-05 04:35:38 -0400

    Fix import { Link }

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 
 const NoteCard = ({ note }) => {
