71ad33c50a2d4f90922a43c6a62ccb0fb0ec2590
Marqui <bmay86@protonmail.com>
2026-04-05 05:13:42 -0400

    Correct Tailwind radial gradient background and layout height

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Routes, Route } from "react-router";
+import { Route, Routes } from "react-router";
+
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
-import "./index.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="relative min-h-screen w-full">
+      <div className="absolute inset-0 -z-10 bg-[radial-gradient(125%_125%_at_50%_10%,#000_60%,rgba(0,255,157,0.25)_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
@@ -16,5 +16,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
