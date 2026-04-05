262f62a70efc45681fe23f3d42b8a5a93cfb518c
Marqui <bmay86@protonmail.com>
2026-04-05 00:52:14 -0400

    Fix daisyui install

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import daisyui from "daisyui";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss({ plugins: [daisyui] })],
+  plugins: [react(), tailwindcss()],
 });
