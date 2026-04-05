093998632340cf4283cf8c5c25a01667e8542402
Marqui <bmay86@protonmail.com>
2026-04-04 23:34:20 -0400

    Test CSS connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,15 @@ import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
 import toast from "react-hot-toast";
+import "./index.css";
 
 const App = () => {
   return (
     <div>
-      <button onClick={() => toast.success("Note created successfully!")}>
+      <button
+        onClick={() => toast.success("Note created successfully!")}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
         Create Note
       </button>
       <Routes>
