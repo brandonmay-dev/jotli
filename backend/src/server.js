ffb2aaccdffe5a5ebcd12ed753e5a81ec0d234e6
Marqui <bmay86@protonmail.com>
2026-04-05 03:13:51 -0400

    Reformat server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-import cors from "cors";
 
 dotenv.config();
 
