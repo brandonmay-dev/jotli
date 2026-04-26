0cd7ecade2c39fb4ec68a3ce9df00d66858d7bd2
Marqui <bmay86@protonmail.com>
2026-04-25 22:45:50 -0400

    focus: prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
@@ -10,6 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(cors());
@@ -18,6 +20,14 @@ app.use(rateLimiter);
 
 // Routes
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFille(path.join(__dirname, "../frontend/dist"));
+  });
+}
 
 // Start server
 const startServer = async () => {
