841b12d09c4aad22ebd0fbf982782270e4ea41af
Marqui <bmay86@protonmail.com>
2026-04-25 23:33:31 -0400

    fix: deployment issues

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
@@ -11,7 +10,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
 // Middleware
 app.use(cors());
@@ -19,15 +17,11 @@ app.use(express.json());
 app.use(rateLimiter);
 
 // Routes
+app.get("/", (_req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.use("/api/notes", notesRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFille(path.join(__dirname, "../frontend/dist"));
-  });
-}
 
 // Start server
 const startServer = async () => {
